159226497c8b3397b18b9dbdb5f446884041c13f
billwartd <billwartd@gmail.com>
2026-06-30 13:59:53 +0530

    index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     
     <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=b05d44a9"></script>
     <script type="text/javascript">
-      var phone_number = '+81 503 114 7654';
+      var phone_number = '0101 866 289 2128';
 
     </script>
 
